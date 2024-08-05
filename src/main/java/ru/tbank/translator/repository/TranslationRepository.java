eb1dfed47b0fdc1a3e1b82899b65b13cb188d0fa
vyacheslavnov03 <vyacheslavnov03@mail.ru>
2024-08-05 12:40:31 +0300

    Добавил модель для предоставления данных репозиторию из сервиса

--- src/main/java/ru/tbank/translator/repository/TranslationRepository.java
+++ src/main/java/ru/tbank/translator/repository/TranslationRepository.java
@@ -3,6 +3,7 @@ package ru.tbank.translator.repository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
+import ru.tbank.translator.model.TranslationModel;
 
 @Repository
 public class TranslationRepository {
@@ -10,8 +11,13 @@ public class TranslationRepository {
     @Autowired
     private JdbcTemplate jdbcTemplate;
 
-    public void saveTranslation(String ipAddress, String inputText, String translatedText) {
+    public void saveTranslation(TranslationModel translationModel) {
         String sql = "INSERT INTO translations (ip_address, input_text, translated_text) VALUES (?, ?, ?)";
-        jdbcTemplate.update(sql, ipAddress, inputText, translatedText);
+        jdbcTemplate.update(
+                sql,
+                translationModel.getIpAddress(),
+                translationModel.getInputText(),
+                translationModel.getTranslatedText()
+        );
     }
 }
